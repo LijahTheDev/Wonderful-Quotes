75b5182df4bc60e72045a08b2c40fe1de2326476
Paul <lijahthedev@gmail.com>
2019-04-02 18:08:17 -0500

    Add & remove quotes & track the amount of quotes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <appHeader :quoteCount="quotes.length" :maxQuotes="maxQuotes"></appHeader>
+    <appNewQuote @quoteAdded="addNewQuote"></appNewQuote>
+    <appQuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></appQuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      quotes: ["My First Quote"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    addNewQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert(
+          "Max Quote Limit Reached! Please consider deleting a Quote!"
+        );
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
   components: {
-    HelloWorld
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+    <appQuote v-for="(quote, i) in quotes" :key="i" @click.native="deleteQuote(i)">{{quote}}</appQuote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote";
+
+export default {
+  name: "QuoteGrid",
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
